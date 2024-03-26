9d36eec1c127991d9b23b5c46cf6179dcc31abf6
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2024-03-26 05:30:17 +0200

    Initial Commit

---
+++ MasgedSilencer/MasgedSilencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasgedSilencerApp.swift
+//  MasgedSilencer
+//
+//  Created by abuzeid on 26.03.24.
+//
+
+import SwiftUI
+
+@main
+struct MasgedSilencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
