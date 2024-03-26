5c46d623cbe98198ac19af2c353f36a359d749c3
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2024-03-26 09:05:32 +0200

    Add Feature flag to prepare version 0.0.1

--- MasgedSilencer/Screens/HomeScreen/ContentView.swift
+++ MasgedSilencer/Screens/HomeScreen/ContentView.swift
@@ -11,36 +11,43 @@ import MapKit
 import SwiftUI
 import UIKit
 
-//
-
-
 struct ContentView: View {
     @EnvironmentObject var router: Router
     private var locationListener = LocationListener()
-    @State var locationManager = LocationManager()
+    @ObservedObject var locationManager = LocationManager()
     var body: some View {
         NavigationStack(path: $router.path) {
             VStack {
                 HStack {
                     Text("Current Location:")
                     if let loc = locationManager.currentLocation {
-                        Text("\(loc.coordinate.latitude)") + Text("\(loc.coordinate.longitude)")
+                        Text("\(loc.coordinate.latitude), \(loc.coordinate.longitude)")
+                    } else {
+                        Text("Waiting for location...")
                     }
                 }
-                if locationListener.isCurrentLocationMosque {
-                    Text("This is a mosque")
-                        .background(.green)
+
+                if FeatureFlag.isEnabled(.addNewMosque) {
+                    Spacer()
+                    Button("Add New Mosque") {
+                        router.path.append(Screen.mapScreen)
+                    }
+                }
+                if FeatureFlag.isEnabled(.showNearbySupportedMosques) {
+                    Spacer()
+                    Button("Show All") {
+                        router.path.append(Screen.mosqueList)
+                    }
                 }
                 Spacer()
-                Button("Add New Mosque") {
-                    router.path.append(Screen.mapScreen)
-                }
-                Spacer()
-                Button("Show All") {
-                    router.path.append(Screen.mosqueList)
-                }
-                Spacer()
+
+                Text(locationListener.isCurrentLocationMosque ? "This is a mosque" : "You are far away from the mosque")
             }
+//            .edgesIgnoringSafeArea(.all)
+
+            .background(locationListener.isCurrentLocationMosque ? Color.green : Color(UIColor.lightGray))
+//            .edgesIgnoringSafeArea(.all)
+
             .navigationDestination(for: Screen.self) { screen in
                 switch screen {
                 case .mapScreen:
@@ -53,11 +60,8 @@ struct ContentView: View {
     }
 }
 
-
 var mosqueRadius: CLLocationDistance = 200
-
 
 #Preview {
     ContentView()
 }
-
